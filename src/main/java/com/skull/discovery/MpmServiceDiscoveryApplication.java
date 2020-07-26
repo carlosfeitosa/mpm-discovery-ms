4d787d86bec4d49e651d7466f342ae2eb491fcf3
Carlos Feitosa <carlos.feitosa.nt@gmail.com>
2020-07-25 23:53:34 -0300

    first commit

---
+++ src/main/java/com/skull/discovery/MpmServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.skull.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpmServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpmServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skull/discovery/MpmServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skull.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpmServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
