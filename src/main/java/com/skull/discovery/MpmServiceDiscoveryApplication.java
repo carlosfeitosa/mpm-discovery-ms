2ffe906c84003a5b713823668fb1a92290f81fe4
Carlos Feitosa <carlos.feitosa.nt@gmail.com>
2020-08-15 00:05:04 -0300

    pmd

--- src/main/java/com/skull/discovery/MpmServiceDiscoveryApplication.java
+++ src/main/java/com/skull/discovery/MpmServiceDiscoveryApplication.java
@@ -4,11 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Eureka server main class.
+ * 
+ * @author Carlos Feitosa (carlos.feitosa.nt@gmail.com)
+ * @since 2020-08-15
+ *
+ */
 @SpringBootApplication
 @EnableEurekaServer
-public class MpmServiceDiscoveryApplication {
+public class MpmServiceDiscoveryApplication { // NOPMD by skull on 8/15/20, 12:03 AM
 
-	public static void main(String[] args) {
+	/**
+	 * Main application's method.
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+
 		SpringApplication.run(MpmServiceDiscoveryApplication.class, args);
 	}
 
--- src/test/java/com/skull/discovery/MpmServiceDiscoveryApplicationTests.java
+++ src/test/java/com/skull/discovery/MpmServiceDiscoveryApplicationTests.java
@@ -3,11 +3,22 @@ package com.skull.discovery;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MpmServiceDiscoveryApplicationTests {
+/**
+ * Test main class for Eureka service.
+ * 
+ * @author Carlos Feitosa (carlos.feitosa.nt@gmail.com)
+ * @since 2020-08-14
+ *
+ */
+@SpringBootTest(classes = MpmServiceDiscoveryApplication.class)
+class MpmServiceDiscoveryApplicationTests { // NOPMD by skull on 8/14/20, 11:58 PM
 
+	/**
+	 * Test if context loads.
+	 */
 	@Test
-	void contextLoads() {
+	public void contextLoads() { // NOPMD by skull on 8/15/20, 12:01 AM
+		// automatic context check
 	}
 
 }
